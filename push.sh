938d3f30a9958d1501888a95c45b32df4fd9d024
Andrew C. Scoppa <andrew-scoppa@github.com>
2024-07-29 09:53:04 -0400

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,10 +1,10 @@
 # This script contains git command to stage, commit and push the changes to the 'main' branch of the remote repository.
 
 # Stage the changes
-git stage .
+git add .
 
 # Commit the changes
-git commit -m "Update"
+git commit -m "Modified content"
 
 # Push the changes to the 'main' branch of the remote repository
 git push origin main
